c0032e527dc5e9a4e7ee768299c57f870c051231
zhouruxuan <zhouruxuan@meituan.com>
2024-11-22 16:57:27 +0800

    first commit

---
+++ src/main/java/com/example/spring5jdk8test/Spring5Jdk8TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring5jdk8test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5Jdk8TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5Jdk8TestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring5jdk8test/Spring5Jdk8TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring5jdk8test;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5Jdk8TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
